ef234078cd4237d8867adf174d7407f7455c84b2
Jorge Morin <jmorin1@hawk.iit.edu>
2023-10-12 17:42:27 -0500

    Can send response of user to chatgpt and thru websocket now

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(bodyParser.json());
 
 const openAIApiKey = process.env.API_KEY_OPEN_AI;
 
+const openai = axios.create({
+  baseURL: "https://api.openai.com/v1",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${openAIApiKey}`,
+  },
+});
+
 const sockserver = new WebSocketServer({ port: 443 });
 sockserver.on("connection", (ws) => {
   console.log("New client connected!");
@@ -18,6 +26,16 @@ sockserver.on("connection", (ws) => {
     console.log("Client has disconnected!");
   });
   ws.on("message", (data) => {
+    sockserver.clients.forEach(async (client) => {
+      let dataString = `${data}`;
+      const userMessageObj = { user: dataString };
+      const userMessage = JSON.stringify(userMessageObj);
+      client.send(userMessage);
+      const aiResponse = await sendChatGPT(dataString);
+      const aiMessageObj = {ai: aiResponse};
+      const aiMessage = JSON.stringify(aiMessageObj);
+      client.send(aiMessage);
+    });
     console.log(`${data}`);
     //createDatabaseAndCollections(`${data}`);
   });
@@ -25,6 +43,23 @@ sockserver.on("connection", (ws) => {
     console.log("websocket error");
   };
 });
+
+async function sendChatGPT(content) {
+  const messages = [{ role: "user", content: content }];
+  return new Promise(async (resolve, reject) => {
+    try {
+      const chatGPTResponse = await openai.post("/chat/completions", {
+        model: "gpt-3.5-turbo",
+        messages,
+      });
+      const replyMessage = chatGPTResponse.data.choices[0].message.content;
+      resolve(replyMessage);
+    } catch (error) {
+      console.error("Error sending message to OpenAI:", error.message);
+      reject(error);
+    }
+  });
+}
 
 const port = 3000;
 app.listen(port, () => {
